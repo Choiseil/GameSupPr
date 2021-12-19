49871a95c02c0e389233b4404d47b4acda165c69
mam675 <mam675@naver.com>
2021-12-20 01:34:57 +0900

    프론트 엔드 틀 제작

--- GameSupPr/FrontEnd/ShowGameForm.Designer.cs
+++ GameSupPr/FrontEnd/ShowGameForm.Designer.cs
@@ -1,12 +1,12 @@
 ﻿
-namespace GameSupPr {
-    partial class Form1 {
-        /// <summary>
+namespace GameSupPr.FrontEnd {
+    partial class ShowGameForm {
+        /// <summary> 
         /// 필수 디자이너 변수입니다.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// 사용 중인 모든 리소스를 정리합니다.
         /// </summary>
         /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
@@ -17,38 +17,25 @@ namespace GameSupPr {
             base.Dispose(disposing);
         }
 
-        #region Windows Form 디자이너에서 생성한 코드
+        #region 구성 요소 디자이너에서 생성한 코드
 
-        /// <summary>
+        /// <summary> 
         /// 디자이너 지원에 필요한 메서드입니다. 
         /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
         /// </summary>
         private void InitializeComponent() {
-            this.panel = new System.Windows.Forms.Panel();
             this.SuspendLayout();
             // 
-            // panel
-            // 
-            this.panel.Location = new System.Drawing.Point(14, 13);
-            this.panel.Name = "panel";
-            this.panel.Size = new System.Drawing.Size(766, 414);
-            this.panel.TabIndex = 0;
-            // 
-            // Form1
+            // ShowGameForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "ShowGameForm";
+            this.Size = new System.Drawing.Size(816, 569);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Panel panel;
     }
 }
-
